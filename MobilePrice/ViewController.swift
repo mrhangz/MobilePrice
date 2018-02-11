7baa88ad672318dc62d77713928c5af9d38773a4
Teerawat Vanasapdamrong <teerawat.v@11street.co.th>
2018-02-11 10:21:45 +0700

    initial commit

---
+++ MobilePrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobilePrice
+//
+//  Created by Teerawat Vanasapdamrong on 2/11/2561 BE.
+//  Copyright © 2561 mrhangz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
